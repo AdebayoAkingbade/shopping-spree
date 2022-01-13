0d40ac2f3eb8413ea2ad89f2f15d11047c50589b
adebayoakingbade <adebayo.akingbade@decagon.dev>
2022-01-13 11:23:48 +0100

    fix register bug not working

--- routes/auth.js
+++ routes/auth.js
@@ -4,20 +4,22 @@ const CryptoJS = require("crypto-js")
 const jwt = require("jsonwebtoken")
 
 router.post("/register", async (req, res)=>{
-    const newUser = new User({
-        username: req.body.username,
-        email: req.body.email,
-        password: CryptoJS.AES.encrypt(
-            req.body.password,
-            process.env.SECRET_PASS
-          ).toString(),
-        // password: CryptoJS.AES.encrypt(req.body.password, process.env.SECRET).toString
-    })
+    const secret = process.env.SECRET_PASS;
+    const reqPassword = req.body.password.toString()
     try{
+        const password = CryptoJS.AES.encrypt(
+            reqPassword,
+            secret
+          ).toString();
+        const newUser = new User({
+            username: req.body.username,
+            email: req.body.email,
+            password,
+        })
     const savedUser= await newUser.save();
     res.status(201).json(savedUser)
     } catch(err){
-        res.status(500).json(err)
+        res.status(500).json(err.message)
     }
 })
 
